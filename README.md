217ffcc9af587b4e6d3fde7ceee63ab7bad3e5c8
williamwoldum <wwoldu21@student.aau.dk>
2024-09-17 23:58:44 +0200

    initial prisma setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AIS-backend
+
+## Development
+- Copy `.env.example` to `.env` and fill out appropiate values
+- If connecting to new db run `npx prisma migrate deploy` to apply migration files 
+- If changes are made to `schema.prisma`
+    - Run `npx prisma migrate dev --name "{...}"` to create and apply new migration file
+    - Run `npx prisma generate` to create/update the prisma client class used in the code
+- `npm i`
+- `npm run dev`
---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+  {
+    ignores: ['dist/', 'node_modules/'],
+  },
+  { files: ['src/**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      quotes: [2, 'single', { avoidEscape: true }],
+      semi: [2, 'never'],
+    },
+  },
+]
