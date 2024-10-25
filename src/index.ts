85e40227a5a9a304899dad4d8ad8eba4f42e9907
williamwoldum <wwoldu21@student.aau.dk>
2024-10-25 13:47:03 +0200

    Env update
    Co-authored-by: Jonas Geertsen Lund <jonasglund@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,16 @@ import { Queue, QueueEvents } from 'bullmq'
 
 dotenv.config()
 
-const { SERVER_PORT, SERVER_IP } = process.env
+const { SERVER_PORT, SERVER_IP, REDIS_IP, REDIS_PORT, REDIS_QUEUE_NAME } = process.env
 
-if (!SERVER_PORT || !SERVER_IP) {
+if (!SERVER_PORT || !SERVER_IP || !REDIS_IP || !REDIS_PORT || !REDIS_QUEUE_NAME) {
   throw new Error('Missing environment variables')
 }
 
 const databaseClient = new PrismaClient()
 const databaseHandler = new PrismaDatabaseHandler(databaseClient)
 const logicHandler = new LogicHandler(databaseHandler)
-const jobQueue = new Queue('AIS', { connection: { host: 'localhost', port: 6379 } })
+const jobQueue = new Queue(REDIS_QUEUE_NAME, { connection: { host: REDIS_IP, port: Number(REDIS_PORT) } })
 const queueEvents = new QueueEvents('foo')
 const jobHandler = new JobHandler(logicHandler, databaseHandler, jobQueue, queueEvents)
 const controller = new GRPCController(jobHandler, logicHandler, databaseHandler)
